7122d3c1142d5820858d7618276a04f0111bf2de
maximos-nn <52293321+maximos-nn@users.noreply.github.com>
2019-08-20 10:56:35 +0300

    Схема БД

---
+++ schema.sql
@@ -0,0 +1,67 @@
+CREATE DATABASE IF NOT EXISTS yeticave
+CHARACTER SET utf8;
+
+USE yeticave;
+
+DROP TABLE IF EXISTS `bids`;
+DROP TABLE IF EXISTS `lots`;
+DROP TABLE IF EXISTS `users`;
+DROP TABLE IF EXISTS `categories`;
+
+CREATE TABLE `categories` (
+    id int unsigned NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `name` char(255) NOT NULL UNIQUE KEY,
+    `code` char(255) NOT NULL UNIQUE KEY
+)  CHARSET=utf8;
+
+CREATE TABLE `users` (
+    `id` int unsigned NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `add_date` datetime NOT NULL DEFAULT NOW(),
+    `email` CHAR(255) NOT NULL,
+    `name` CHAR(255) NOT NULL,
+    `password` CHAR(255) NOT NULL,
+    `avatar_path` CHAR(255),
+    `contact` TEXT
+)  CHARSET=utf8;
+
+CREATE TABLE `lots` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `add_date` datetime NOT NULL DEFAULT NOW(),
+  `title` char(255) NOT NULL,
+  `description` text,
+  `image_path` char(255) NOT NULL,
+  `price` decimal(10,2) NOT NULL,
+  `expire_date` datetime NOT NULL,
+  `bid_step` int unsigned NOT NULL,
+  `user_id` int unsigned NOT NULL,
+  `winner_id` int unsigned DEFAULT NULL,
+  `category_id` int unsigned NOT NULL,
+  KEY `idx_fk_lots_user` (`user_id`),
+  KEY `idx_fk_lots_winner` (`winner_id`),
+  KEY `idx_fk_lots_cat` (`category_id`),
+  FULLTEXT KEY `idx_lots_title` (`title`),
+  FULLTEXT KEY `idx_lots_description` (`description`),
+  CONSTRAINT `fk_lots_user` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_lots_winner` FOREIGN KEY (`winner_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_lots_cat` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) CHARSET=utf8;
+
+CREATE TABLE `bids` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `add_date` datetime NOT NULL DEFAULT NOW(),
+  `amount` int unsigned NOT NULL,
+  `user_id` int unsigned NOT NULL,
+  `lot_id` int unsigned NOT NULL,
+  KEY `idx_fk_bids_user` (`user_id`),
+  KEY `idx_fk_bids_lot` (`lot_id`),
+  CONSTRAINT `fk_bids_user` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
+  CONSTRAINT `fk_bids_lot` FOREIGN KEY (`lot_id`) REFERENCES `lots` (`id`)
+) CHARSET=utf8;
+
+INSERT INTO `categories` (`name`, `code`)
+VALUES ('Доски и лыжи', 'boards'),
+('Крепления', 'attachment'),
+('Ботинки', 'boots'),
+('Одежда', 'clothing'),
+('Инструменты', 'tools'),
+('Разное', 'other');
