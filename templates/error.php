27681418ccd8ed64772b2623a3f571a6e802362f
maximos-nn <52293321+maximos-nn@users.noreply.github.com>
2019-08-25 22:14:36 +0300

    Чтение и показ записей из MySQL

--- templates/error.php
+++ templates/error.php
@@ -2,61 +2,48 @@
 <html lang="ru">
 <head>
     <meta charset="UTF-8">
-    <title><?= $pageTitle ?></title>
-    <link href="/css/normalize.min.css" rel="stylesheet">
-    <link href="/css/style.css" rel="stylesheet">
+    <title>Ошибка</title>
+    <link href="../css/normalize.min.css" rel="stylesheet">
+    <link href="../css/style.css" rel="stylesheet">
 </head>
 <body>
+
 <div class="page-wrapper">
 
-<header class="main-header">
-    <div class="main-header__container container">
-        <h1 class="visually-hidden">YetiCave</h1>
-        <a class="main-header__logo">
-            <img src="/img/logo.svg" width="160" height="39" alt="Логотип компании YetiCave">
-        </a>
-        <form class="main-header__search" method="get" action="https://echo.htmlacademy.ru" autocomplete="off">
-            <input type="search" name="search" placeholder="Поиск лота">
-            <input class="main-header__search-btn" type="submit" name="find" value="Найти">
-        </form>
-        <a class="main-header__add-lot button" href="pages/add-lot.html">Добавить лот</a>
-
-        <nav class="user-menu">
-            <?php if ($is_auth): ?>
-                <div class="user-menu__logged">
-                    <p><?= $user_name ?></p>
-                    <a class="user-menu__bets" href="pages/my-bets.html">Мои ставки</a>
-                    <a class="user-menu__logout" href="#">Выход</a>
-                </div>
-            <?php else: ?>
+    <header class="main-header">
+        <div class="main-header__container container">
+            <h1 class="visually-hidden">YetiCave</h1>
+            <a class="main-header__logo" href="index.html">
+                <img src="../img/logo.svg" width="160" height="39" alt="Логотип компании YetiCave">
+            </a>
+            <form class="main-header__search" method="get" action="https://echo.htmlacademy.ru" autocomplete="off">
+                <input type="search" name="search" placeholder="Поиск лота">
+                <input class="main-header__search-btn" type="submit" name="find" value="Найти">
+            </form>
+            <a class="main-header__add-lot button" href="add-lot.html">Добавить лот</a>
+            <nav class="user-menu">
                 <ul class="user-menu__list">
                     <li class="user-menu__item">
-                        <a href="#">Регистрация</a>
+                        <a href="sign-up.html">Регистрация</a>
                     </li>
                     <li class="user-menu__item">
-                        <a href="#">Вход</a>
+                        <a href="login.html">Вход</a>
                     </li>
                 </ul>
-            <?php endif; ?>
-        </nav>
-    </div>
-</header>
+            </nav>
+        </div>
+    </header>
 
-<main class="container">
-    <?= $mainContent ?>
-</main>
+    <main>
+        <section class="lot-item container">
+            <h2>Ошибка</h2>
+            <p><?= $error ?></p>
+        </section>
+    </main>
+
 </div>
 
 <footer class="main-footer">
-    <nav class="nav">
-        <ul class="nav__list container">
-            <?php foreach ($categories as $category): ?>
-                <li class="nav__item">
-                    <a href="pages/all-lots.html"><?= clearSpecials($category['name']) ?></a>
-                </li>
-            <?php endforeach; ?>
-        </ul>
-    </nav>
     <div class="main-footer__bottom container">
         <div class="main-footer__copyright">
             <p>© 2019, YetiCave</p>
@@ -99,7 +86,5 @@
     </div>
 </footer>
 
-<script src="flatpickr.js"></script>
-<script src="script.js"></script>
 </body>
 </html>
