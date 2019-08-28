c530c452ce2ea8dc4d1f1b51f47bfa29362df238
maximos-nn <52293321+maximos-nn@users.noreply.github.com>
2019-08-28 10:43:25 +0300

    Реорганизуем общий установочный код

--- index.php
+++ index.php
@@ -1,16 +1,5 @@
 <?php
-$is_auth = rand(0, 1);
-
-$user_name = 'maximos';
-
-date_default_timezone_set('Europe/Moscow');
-require_once 'functions/template.php';
-require_once 'functions/db.php';
-
-if (!file_exists('config.php')) {
-    exit('Создайте файл config.php на основе config.sample.php и выполните настройку.');
-}
-$config = require 'config.php';
+require 'bootstrap.php';
 
 $dbConnection = dbConnect($config['db']);
 
