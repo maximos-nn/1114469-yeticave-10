d12cbebfbd69e3f912bfc1ca414b376d0644f008
maximos-nn <52293321+maximos-nn@users.noreply.github.com>
2019-09-04 13:58:38 +0300

    Упростили вывод ошибок в шаблонах

--- templates/add-lot.php
+++ templates/add-lot.php
@@ -6,7 +6,7 @@
       <div class="form__item <?= isset($errors['lot-name']) ? 'form__item--invalid' : '' ?>">
         <label for="lot-name">Наименование <sup>*</sup></label>
         <input id="lot-name" type="text" name="lot-name" placeholder="Введите наименование лота" value="<?= clearSpecials($form['lot-name'] ?? '') ?>">
-        <span class="form__error"><?= isset($errors['lot-name']) ? $errors['lot-name'] : '' ?></span>
+        <span class="form__error"><?= $errors['lot-name'] ?? '' ?></span>
       </div>
       <div class="form__item <?= isset($errors['category']) ? 'form__item--invalid' : '' ?>">
         <label for="category">Категория <sup>*</sup></label>
@@ -18,13 +18,13 @@
             </option>
           <?php endforeach; ?>
         </select>
-        <span class="form__error"><?= isset($errors['category']) ? $errors['category'] : '' ?></span>
+        <span class="form__error"><?= $errors['category'] ?? '' ?></span>
       </div>
     </div>
     <div class="form__item form__item--wide <?= isset($errors['message']) ? 'form__item--invalid' : '' ?>">
       <label for="message">Описание <sup>*</sup></label>
       <textarea id="message" name="message" placeholder="Напишите описание лота"><?= clearSpecials($form['message'] ?? '') ?></textarea>
-      <span class="form__error"><?= isset($errors['message']) ? $errors['message'] : '' ?></span>
+      <span class="form__error"><?= $errors['message'] ?? '' ?></span>
     </div>
     <div class="form__item form__item--file <?= isset($errors['lot-img']) ? 'form__item--invalid' : '' ?>">
       <label>Изображение <sup>*</sup></label>
@@ -33,24 +33,24 @@
         <label for="lot-img">
           Добавить
         </label>
-        <span class="form__error"><?= isset($errors['lot-img']) ? $errors['lot-img'] : '' ?></span>
+        <span class="form__error"><?= $errors['lot-img'] ?? '' ?></span>
       </div>
     </div>
     <div class="form__container-three">
       <div class="form__item form__item--small <?= isset($errors['lot-rate']) ? 'form__item--invalid' : '' ?>">
         <label for="lot-rate">Начальная цена <sup>*</sup></label>
         <input id="lot-rate" type="text" name="lot-rate" placeholder="0" value="<?= clearSpecials($form['lot-rate'] ?? '') ?>">
-        <span class="form__error"><?= isset($errors['lot-rate']) ? $errors['lot-rate'] : '' ?></span>
+        <span class="form__error"><?= $errors['lot-rate'] ?? '' ?></span>
       </div>
       <div class="form__item form__item--small <?= isset($errors['lot-step']) ? 'form__item--invalid' : '' ?>">
         <label for="lot-step">Шаг ставки <sup>*</sup></label>
         <input id="lot-step" type="text" name="lot-step" placeholder="0" value="<?= clearSpecials($form['lot-step'] ?? '') ?>">
-        <span class="form__error"><?= isset($errors['lot-step']) ? $errors['lot-step'] : '' ?></span>
+        <span class="form__error"><?= $errors['lot-step'] ?? '' ?></span>
       </div>
       <div class="form__item <?= isset($errors['lot-date']) ? 'form__item--invalid' : '' ?>">
         <label for="lot-date">Дата окончания торгов <sup>*</sup></label>
         <input class="form__input-date" id="lot-date" type="text" name="lot-date" placeholder="Введите дату в формате ГГГГ-ММ-ДД" value="<?= clearSpecials($form['lot-date'] ?? '') ?>">
-        <span class="form__error"><?= isset($errors['lot-date']) ? $errors['lot-date'] : '' ?></span>
+        <span class="form__error"><?= $errors['lot-date'] ?? '' ?></span>
       </div>
     </div>
     <span class="form__error form__error--bottom">Пожалуйста, исправьте ошибки в форме.</span>
--- templates/sign-up.php
+++ templates/sign-up.php
@@ -5,22 +5,22 @@
     <div class="form__item <?= isset($errors['email']) ? 'form__item--invalid' : '' ?>">
       <label for="email">E-mail <sup>*</sup></label>
       <input id="email" type="text" name="email" placeholder="Введите e-mail" value="<?= clearSpecials($form['email'] ?? '') ?>">
-      <span class="form__error"><?= isset($errors['email']) ? $errors['email'] : '' ?></span>
+      <span class="form__error"><?= $errors['email'] ?? '' ?></span>
     </div>
     <div class="form__item <?= isset($errors['password']) ? 'form__item--invalid' : '' ?>">
       <label for="password">Пароль <sup>*</sup></label>
       <input id="password" type="password" name="password" placeholder="Введите пароль" value="<?= clearSpecials($form['password'] ?? '') ?>">
-      <span class="form__error"><?= isset($errors['password']) ? $errors['password'] : '' ?></span>
+      <span class="form__error"><?= $errors['password'] ?? '' ?></span>
     </div>
     <div class="form__item <?= isset($errors['name']) ? 'form__item--invalid' : '' ?>">
       <label for="name">Имя <sup>*</sup></label>
       <input id="name" type="text" name="name" placeholder="Введите имя" value="<?= clearSpecials($form['name'] ?? '') ?>">
-      <span class="form__error"><?= isset($errors['name']) ? $errors['name'] : '' ?></span>
+      <span class="form__error"><?= $errors['name'] ?? '' ?></span>
     </div>
     <div class="form__item <?= isset($errors['message']) ? 'form__item--invalid' : '' ?>">
       <label for="message">Контактные данные <sup>*</sup></label>
       <textarea id="message" name="message" placeholder="Напишите как с вами связаться"><?= clearSpecials($form['message'] ?? '') ?></textarea>
-      <span class="form__error"><?= isset($errors['message']) ? $errors['message'] : '' ?></span>
+      <span class="form__error"><?= $errors['message'] ?? '' ?></span>
     </div>
     <span class="form__error form__error--bottom">Пожалуйста, исправьте ошибки в форме.</span>
     <button type="submit" class="button">Зарегистрироваться</button>
