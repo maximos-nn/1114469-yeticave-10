3cd0a115add5b943c39f79d130d7b4c86abf61ae
maximos-nn <52293321+maximos-nn@users.noreply.github.com>
2019-09-10 08:01:19 +0300

    Доработана постраничная навигация

--- templates/pagination.php
+++ templates/pagination.php
@@ -1,11 +1,21 @@
 <?php if ($pages): ?>
 <ul class="pagination-list">
-  <li class="pagination-item pagination-item-prev"><a>Назад</a></li>
+  <li class="pagination-item pagination-item-prev">
+    <a <?= ($currentPage - 1) ? 'href="' . $script . '?page=' .  ($currentPage - 1) . ($queryFields ? '&' . $queryFields : '') . '"' : '' ?>>
+      Назад
+    </a>
+  </li>
   <?php foreach ($pages as $page): ?>
   <li class="pagination-item <?= $currentPage === $page ? 'pagination-item-active' : '' ?>">
-    <a <?= $currentPage !== $page ? $script . '?page=' . $page . ($queryFields ? '&' . $queryFields : '') : '' ?>><?= $page ?></a>
+    <a <?= $currentPage !== $page ? 'href="' . $script . '?page=' . $page . ($queryFields ? '&' . $queryFields : '') . '"' : '' ?>>
+      <?= $page ?>
+    </a>
   </li>
   <?php endforeach; ?>
-  <li class="pagination-item pagination-item-next"><a href="#">Вперед</a></li>
+  <li class="pagination-item pagination-item-next">
+    <a <?= isset($pages[$currentPage + 1]) ? 'href="' . $script . '?page=' .  ($currentPage + 1) . ($queryFields ? '&' . $queryFields : '') . '"' : '' ?>>
+      Вперед
+    </a>
+  </li>
 </ul>
 <?php endif; ?>
