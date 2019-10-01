d9317fffc7dd94586f7804da98d4db1299dd45c2
maximos-nn <52293321+maximos-nn@users.noreply.github.com>
2019-10-01 11:24:04 +0300

    Подключаем функции для работы с БД

--- bootstrap.php
+++ bootstrap.php
@@ -6,7 +6,13 @@ $sessUser = $_SESSION['user'] ?? null;
 
 date_default_timezone_set('Europe/Moscow');
 require __DIR__ . '/functions/template.php';
-require __DIR__ . '/functions/db.php';
+require __DIR__ . '/functions/db/common.php';
+require __DIR__ . '/functions/db/categories.php';
+require __DIR__ . '/functions/db/lots.php';
+require __DIR__ . '/functions/db/users.php';
+require __DIR__ . '/functions/db/bids.php';
+require __DIR__ . '/functions/db/search.php';
+require __DIR__ . '/functions/db/winners.php';
 require __DIR__ . '/functions/validate.php';
 
 if (!file_exists('config.php')) {
